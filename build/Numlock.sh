7536e5d70c7b47cd972c395bb3312b5ce60de199
Joseph Harriott <trohib@gmail.com>
2019-03-24 19:33:22 +0100

    reject

--- build/Numlock.sh
+++ build/Numlock.sh
@@ -6,3 +6,4 @@
 mkdir /etc/systemd/system/getty@.service.d
 echo [Service] > /etc/systemd/system/getty@.service.d/activate-numlock.conf
 echo "ExecStartPre=/bin/sh -c 'setleds +num < /dev/%I'" >> /etc/systemd/system/getty@.service.d/activate-numlock.conf
+cat /etc/systemd/system/getty@.service.d/activate-numlock.conf
--- build/clone.sh
+++ build/clone.sh
@@ -5,4 +5,5 @@
 
 # this requires authenticating...
 # git clone git@github.com:harriott/AcerVeritonT661.git
+
 git clone https://github.com/harriott/AcerVeritonT661.git
