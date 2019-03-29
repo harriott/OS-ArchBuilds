05e90d91d95570be586cae94efa65fca8a0981e0
Joseph Harriott <trohib@gmail.com>
2019-03-29 11:34:56 +0100

    mkdir \mnt\9QG2FFEE

--- build/fstabLABELs.sh
+++ build/fstabLABELs.sh
@@ -4,6 +4,9 @@
 # bash -x <thisfile>.sh
 
 # HD LABELs for fstab
+mkdir \mnt\9QG2FFEE
+mkdir \mnt\HD103SJ
+mkdir \mnt\WD1001FALS
 echo "LABEL=9QG2FFEE /mnt/9QG2FFEE  ext4  defaults  0  2" >> /etc/fstab
 # echo "LABEL=HD103SJ /mnt/HD103SJ  ext4  defaults  0  2" >> /etc/fstab
 # echo "LABEL=WD1001FALS /mnt/WD1001FALS  ext4  defaults  0  2" >> /etc/fstab
