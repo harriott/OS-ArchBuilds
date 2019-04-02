13540a2e3812538af6c921eeef90ed1e2735d92c
Joseph Harriott <trohib@gmail.com>
2019-04-02 13:33:25 +0200

    backdest 9QG2FFEE

---
+++ build/rsyncBackup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# vim: set et tw=0:
+
+# Full system backup
+# ------------------
+
+# Joseph Harriott, Tue 02 Apr 2019
+
+# Run this script from a Live ISO:  bash /mnt/home/jo/Arch/rsyncBackup.sh
+
+# Backup destination
+backdest=/mnt/mnt/9QG2FFEE
+
+# Labels for backup name
+date=$(date "+%F-%H-%M")
+bfolder="$backdest/$date"
+echo "Intending to rsync-backup of some of what's on /mnt to $bfolder"
+
+# Check, then go:
+echo -n "You have booted from a Live ISO, and are you ready now to backup? (y/N): "
+read gobu
+if [ $gobu ]; then
+  if [ $gobu = "y" ]; then
+    # Check if target directory exists
+    if [ ! -d $backdest ]; then
+      echo "The target directory ain't there, quitting."
+      exit
+    fi
+    echo "rsync-ing:"
+    mkdir $bfolder
+    for sysfolder in boot etc root usr var; do
+      mkdir $bfolder/$sysfolder
+      rsync -aAivX /mnt/$sysfolder/ $bfolder/$sysfolder
+    done
+  fi
+fi
