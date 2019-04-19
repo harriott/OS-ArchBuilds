d0143959240d0c26c66c780df530afe70b4cc530
Joseph Harriott <trohib@gmail.com>
2019-04-19 22:05:19 +0200

    obmenu-generator

---
+++ build/obmenu-generator.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# vim: set et tw=0:
+
+# bash -x <thisfile>.sh
+
+# obmenu-generator
+git clone https://aur.archlinux.org/packages/auracle-git
+git clone https://aur.archlinux.org/packages/perl-linux-desktopfiles/
+git clone https://aur.archlinux.org/packages/obmenu-generator/
+
