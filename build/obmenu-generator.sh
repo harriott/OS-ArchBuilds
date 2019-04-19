b4601f3d8438c1576439d1f8cf2781405acf3dc0
Joseph Harriott <trohib@gmail.com>
2019-04-19 23:24:09 +0200

    fixed git clone URLs

--- build/obmenu-generator.sh
+++ build/obmenu-generator.sh
@@ -4,7 +4,7 @@
 # bash -x <thisfile>.sh
 
 # obmenu-generator
-git clone https://aur.archlinux.org/packages/auracle-git
-git clone https://aur.archlinux.org/packages/perl-linux-desktopfiles/
-git clone https://aur.archlinux.org/packages/obmenu-generator/
+git clone https://aur.archlinux.org/auracle-git.git
+git clone https://aur.archlinux.org/perl-linux-desktopfiles.git
+git clone https://aur.archlinux.org/obmenu-generator.git
 
