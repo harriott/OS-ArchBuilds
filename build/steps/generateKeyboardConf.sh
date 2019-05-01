70f29b801cdd8f6db06a23cad7fd19eae6e211e2
Joseph Harriott <trohib@gmail.com>
2019-05-01 17:02:49 +0200

    generateKeyboardConf

---
+++ build/steps/generateKeyboardConf.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# vim: set et tw=0:
+
+# sudo bash -x <thisfile>.sh
+
+# generate 00-keyboard.conf
+localectl --no-convert set-x11-keymap gb logitech_base
+
--- build/steps/openboxAppearance.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# vim: set et tw=0:
-
-# sudo bash -x <thisfile>.sh
-
-# Openbox appearance
-pacman -S lxappearance-obconf obconf ttf-ubuntu-font-family
-
