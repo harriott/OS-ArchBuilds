a4c632f8b83c2e7c208343240b50575038ff2dcc
Joseph Harriott <trohib@gmail.com>
2019-05-02 10:45:12 +0200

    mimeo-gpg

--- build/steps/mimeo.sh
+++ build/steps/mimeo.sh
@@ -3,10 +3,7 @@
 
 # bash -x <thisfile>.sh
 
-# obmenu-generator
-cp ~/AcerVeritonT661/configFiles/autostart ~/.config/openbox
-cp ~/AcerVeritonT661/configFiles/schema.pl ~/.config/obmenu-generator
-cp ~/AcerVeritonT661/configFiles/openbox/rx.xml ~/.config/openbox
+# mimeo
 git clone https://aur.archlinux.org/mimeo.git
 git clone https://aur.archlinux.org/openbox-themes.git
 git clone https://aur.archlinux.org/xdg-utils-mimeo.git
