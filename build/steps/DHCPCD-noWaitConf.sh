fddf45a7bc7d7e896dd6cb51755be6a6724a1082
Joseph Harriott <trohib@gmail.com>
2019-05-24 11:46:31 +0200

    dhcpcd@.service.d

--- build/steps/DHCPCD-noWaitConf.sh
+++ build/steps/DHCPCD-noWaitConf.sh
@@ -4,5 +4,6 @@
 # sudo bash -x <thisfile>.sh
 
 # turn off -w flag in dhcpcd@.service
+mkdir /etc/systemd/system/dhcpcd@.service.d
 cp ../../configFiles/etc/no-wait.conf /etc/systemd/system/dhcpcd@.service.d/no-wait.conf
 
