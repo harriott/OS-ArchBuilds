6bad51ec027745124fc655216241c9d505f357b7
Joseph Harriott <trohib@gmail.com>
2019-05-24 14:30:38 +0200

    bashConfig

---
+++ root/bashConfig.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# vim: set et tw=0:
+
+# sudo bash -x <thisfile>.sh
+
+# get my root bash configuration fully working, including in Console
+cp ~/AcerVeritonT661/root/bash_profile ~/.bash_profile
+cp ~/AcerVeritonT661/root/bashrc ~/.bashrc
+
