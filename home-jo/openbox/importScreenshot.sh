079ef93e1f19263df212164a00feb89ca73363b6
Joseph Harriott <trohib@gmail.com>
2019-09-16 21:05:10 +0200

    importScreenshot

---
+++ home-jo/openbox/importScreenshot.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# vim: set et tw=0:
+
+# Take a time-stamped fullscreenshot
+# ----------------------------------
+# Called by  Openbox's  rc.xml  because:
+# <command>importScreenshot</command>  doesn't find my Bash function
+# and the following line doesn't get correctly parsed either.
+
+import -window root "screenshot-$(date '+%j-%H%M%S').jpg"
+
+# This file (or a localised copy of it) needs to be executable (eg 755) and not a symlink.
