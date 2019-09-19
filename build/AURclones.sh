701b5785000448316f174b8656d08af052347f33
Joseph Harriott <trohib@gmail.com>
2019-09-19 17:18:01 +0200

    dropbox.git

--- build/AURclones.sh
+++ build/AURclones.sh
@@ -3,11 +3,12 @@
 
 # sudo bash -x <thisfile>.sh
 
-git clone https://aur.archlinux.org/vim-colors-solarized-git.git
-git clone https://aur.archlinux.org/vim-gruvbox-git.git
-git clone https://aur.archlinux.org/vim-languagetool.git
-git clone https://aur.archlinux.org/vim-mediawiki.git
-git clone https://aur.archlinux.org/vim-repeat.git
-git clone https://aur.archlinux.org/vim-simpylfold.git
-git clone https://aur.archlinux.org/vim-table-mode.git
-git clone https://aur.archlinux.org/vim-undotree.git
+git clone https://aur.archlinux.org/dropbox.git
+# git clone https://aur.archlinux.org/vim-colors-solarized-git.git
+# git clone https://aur.archlinux.org/vim-gruvbox-git.git
+# git clone https://aur.archlinux.org/vim-languagetool.git
+# git clone https://aur.archlinux.org/vim-mediawiki.git
+# git clone https://aur.archlinux.org/vim-repeat.git
+# git clone https://aur.archlinux.org/vim-simpylfold.git
+# git clone https://aur.archlinux.org/vim-table-mode.git
+# git clone https://aur.archlinux.org/vim-undotree.git
