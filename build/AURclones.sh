cc263baac2ebb848e18eb1d31e1a29e8fcb1c171
Joseph Harriott <trohib@gmail.com>
2019-10-07 21:08:50 +0200

    symlink for zathurarc

--- build/AURclones.sh
+++ build/AURclones.sh
@@ -3,9 +3,8 @@
 
 # sudo bash -x <thisfile>.sh
 
-# sudo rm -r dropbox; git clone https://aur.archlinux.org/dropbox.git
+# git clone https://aur.archlinux.org/pnmixer-gtk3.git
 # git clone https://aur.archlinux.org/vim-colors-solarized-git.git
-git clone https://aur.archlinux.org/gtkhash.git
 # git clone https://aur.archlinux.org/vim-gruvbox-git.git
 # git clone https://aur.archlinux.org/vim-languagetool.git
 # git clone https://aur.archlinux.org/vim-mediawiki.git
@@ -13,3 +12,7 @@ git clone https://aur.archlinux.org/gtkhash.git
 # git clone https://aur.archlinux.org/vim-simpylfold.git
 # git clone https://aur.archlinux.org/vim-table-mode.git
 # git clone https://aur.archlinux.org/vim-undotree.git
+# sudo rm -r dropbox; git clone https://aur.archlinux.org/dropbox.git
+# sudo rm -r gtkhash # clone https://aur.archlinux.org/gtkhash.git
+# sudo rm -r xdg-utils-mimeo; git clone https://aur.archlinux.org/xdg-utils-mimeo.git
+# git clone https://aur.archlinux.org/vim-recover-git.git
