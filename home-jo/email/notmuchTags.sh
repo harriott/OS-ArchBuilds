b73acba0005bf8e4fef3a37fef8b32768cedc465
Joseph Harriott <trohib@gmail.com>
2020-01-24 16:56:33 +0100

    regex for non-inbox tags

--- home-jo/email/notmuchTags.sh
+++ home-jo/email/notmuchTags.sh
@@ -4,50 +4,7 @@
 # reset my notmuch tags
 # ---------------------
 
-# fm/Archive:
-notmuch tag +archive -- folder:fm/Archive and not tag:archive
-notmuch tag +fm -- folder:fm/Archive and not tag:fm
-notmuch tag -inbox -- folder:fm/Archive and tag:inbox
-notmuch tag -unread -- folder:fm/Archive and tag:unread
-
-# fm/Cafezoide:
-notmuch tag +cafezoide -- folder:fm/Cafezoide and not tag:cafezoide
-notmuch tag +fm -- folder:fm/Cafezoide and not tag:fm
-notmuch tag -inbox -- folder:fm/Cafezoide and tag:inbox
-notmuch tag -unread -- folder:fm/Cafezoide and tag:unread
-
-# fm/Deauram:
-notmuch tag +deauram -- folder:fm/Deauram and not tag:deauram
-notmuch tag +fm -- folder:fm/Deauram and not tag:fm
-notmuch tag -inbox -- folder:fm/Deauram and tag:inbox
-notmuch tag -unread -- folder:fm/Deauram and tag:unread
-
-# fm/Friends:
-notmuch tag +friends -- folder:fm/Friends and not tag:friends
-notmuch tag +fm -- folder:fm/Friends and not tag:fm
-notmuch tag -inbox -- folder:fm/Friends and tag:inbox
-notmuch tag -unread -- folder:fm/Friends and tag:unread
-
-# fm/INBOX:
-notmuch tag +fm -- folder:fm/INBOX and not tag:fm
-
-# fm/Sent Items:
-notmuch tag +fm -- 'folder:"fm/Sent Items" and not tag:fm'
-notmuch tag -inbox -- 'folder:"fm/Sent Items" and tag:inbox'
-notmuch tag +sent -- 'folder:"fm/Sent Items" and not tag:sent'
-
-# fm/Waiting:
-notmuch tag +fm -- folder:fm/Waiting and not tag:fm
-notmuch tag -inbox -- folder:fm/Waiting and tag:inbox
-notmuch tag -unread -- folder:fm/Waiting and tag:unread
-notmuch tag +waiting -- folder:fm/Waiting and not tag:waiting
-
-# gm/Work:
-notmuch tag +work -- folder:gm/Work and not tag:work
-notmuch tag +gm -- folder:gm/Work and not tag:gm
-notmuch tag -archive -- folder:gm/Work and tag:archive
-notmuch tag -inbox -- folder:gm/Work and tag:inbox
-notmuch tag -unread -- folder:gm/Work and tag:unread
+# check the effects with  notmuch dump
 
 # gmx/Archives:
 notmuch tag +archive -- folder:gmx/Archives and not tag:archive
@@ -75,6 +32,8 @@ notmuch tag +gmx -- folder:gmx/Waiting and not tag:gmx
 notmuch tag -inbox -- folder:gmx/Waiting and tag:inbox
 notmuch tag -unread -- folder:gmx/Waiting and tag:unread
 notmuch tag +waiting -- folder:gmx/Waiting and not tag:waiting
+
+# clear all +live tags with  notmuch tag -live -- "*"
 
 # live/Archive:
 notmuch tag +archive -- folder:live/Archive and not tag:archive
