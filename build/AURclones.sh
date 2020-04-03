05cdc8e256bb9c12f4b5a5835537b7484b8a44cc
Joseph Harriott <trohib@gmail.com>
2020-04-03 12:12:39 +0200

    set termguicolors

--- build/AURclones.sh
+++ build/AURclones.sh
@@ -5,6 +5,8 @@
 
 # git clone https://aur.archlinux.org/autojump.git
 # git clone https://aur.archlinux.org/autojump-git.git
+# git clone https://aur.archlinux.org/cmus-notify.git
+# git clone https://aur.archlinux.org/covid19-cli-git.git
 # git clone https://aur.archlinux.org/figlet-fonts.git
 # git clone https://aur.archlinux.org/iscan-plugin-gt-f500.git
 # git clone https://aur.archlinux.org/moar.git
@@ -24,11 +26,12 @@
 # git clone https://aur.archlinux.org/vim-simpylfold.git
 # git clone https://aur.archlinux.org/vim-table-mode.git
 # git clone https://aur.archlinux.org/xnviewmp.git
+git clone https://aur.archlinux.org/python-notify2.git
 
 # sudo rm -r auracle-git; git clone https://aur.archlinux.org/auracle-git.git
 # sudo rm -r chromium-widevine; git clone https://aur.archlinux.org/chromium-widevine.git
 # sudo rm -r clamav-unofficial-sigs; git clone https://aur.archlinux.org/clamav-unofficial-sigs
-sudo rm -r dropbox; git clone https://aur.archlinux.org/dropbox.git
+# sudo rm -r dropbox; git clone https://aur.archlinux.org/dropbox.git
 # sudo rm -r dust; git clone https://aur.archlinux.org/dust.git
 # sudo rm -r gtkhash # clone https://aur.archlinux.org/gtkhash.git
 # sudo rm -r mimeo; git clone https://aur.archlinux.org/mimeo.git
--- home-jo/Vim/init.vim
+++ home-jo/Vim/init.vim
@@ -2,5 +2,6 @@
 " share my gVim configuration
 set runtimepath^=~/.vim runtimepath+=~/.vim/after
 let &packpath = &runtimepath
+set termguicolors " for accurate colour codes, can turn off with :se notgc
 source ~/.vimrc
 
