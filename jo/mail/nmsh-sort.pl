e1299021f88370ede53cbd772d7a75cb1b53244e
Joseph Harriott <trohib@gmail.com>
2020-06-22 11:24:24 +0200

    sorted

--- jo/mail/nmsh-sort.pl
+++ jo/mail/nmsh-sort.pl
@@ -15,31 +15,35 @@ use feature 'say';
 tie my @nmsh, 'Tie::File', "$ARGV[0]" or die "Can't read file: $!\n";
 # exit;
 
+my @emailsSorted = splice(@nmsh,0,2); # save vim modeline
+
 # mark and join up each email into one single line
 # ------------------------------------------------
-my @nmshSorted = splice(@nmsh,0,2); # save vim modeline
-
 my $allAsLine = join '◙', @nmsh;  # concatenate all lines with ◙ marker between
 $allAsLine =~ s/◙Date: /▶Date: /g;  # mark out each email
 my @emailsAsLines = split /▶/, $allAsLine;  # separate into array
 
-# prefix with Unix time
-my @EALs;
+# prefix with Unix time, then sort
+# --------------------------------
+my @EALunixtime;
 my $date;
 my $emailAsLine;
 foreach $emailAsLine (@emailsAsLines) {
   $date = substr($emailAsLine,6,31);  # get the RFC-822 date provided by notmuch
-  push @EALs, str2time($date).$emailAsLine;
+  push @EALunixtime, str2time($date).$emailAsLine;
 }
-# p @EALs; # Data::Printer - for debugging
+# p @EALunixtime; # Data::Printer - for debugging
+my @EALUTSorted = sort @EALunixtime;
 
-# unmark and restore readable emails, sorted by date
-# --------------------------------------------------
-my @EALsSorted = sort @EALs;
+# unmark and restore readable emails
+# ----------------------------------
 my $EAL;
-foreach $EAL (@EALs) {
+my $EALUTS;
+foreach $EALUTS (@EALUTSorted) {
+  $EAL = substr($EALUTS,10);
+  push(@emailsSorted, $EAL);
 }
 
-@nmsh = @EALsSorted;
+@nmsh = @emailsSorted;
 untie @nmsh;
 
