9535211e29c01588fab7cd5af86461d2aa85dd77
Joseph Harriott <trohib@gmail.com>
2020-06-30 12:43:51 +0200

    fstab

--- build-scripts/08-beforeWM.sh
+++ build-scripts/08-beforeWM.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# vim: fdm=expr fdc=1 ft=shbuild:
+# vim: fdm=expr fdc=1 fdl=1 ft=shbuild:
 
 set -v  # prints each statement here, including comments
 trap read debug  # puts a read request after each executable line
@@ -7,18 +7,18 @@ trap read debug  # puts a read request after each executable line
 #=> make AUR directory
 mkdir ~/Arch/AUR
 
-#=> rsnapshot conf, for first run
+#=> grab default rsnapshot conf
 # grab the default
-cp /etc/rsnapshot.conf $MACHINE/etc/k5c-rsnapshot.conf
-cp /etc/rsnapshot.conf $MACHINE/etc/rsnapshot.conf
+cp /etc/rsnapshot.conf /bs/k6u-rsnapshot.conf
 
-# my configuration
-mkdir /mnt/HD103SJ/rsnapshot
-sudo cp $MACHINE/etc/rsnapshot.conf /etc/rsnapshot.conf
+# #=> rsnapshot conf, for first run
+# # my configuration
+# mkdir /mnt/HD103SJ/rsnapshot
+# sudo cp $MACHINE/etc/rsnapshot.conf /etc/rsnapshot.conf
 
-# check
-rsnapshot configtest
-# simulate first run
-rsnapshot -t hourly
-# now need to  sudo rsnapshot hourly  and monitor progress with  du -sh rsnapshot
+# # check
+# rsnapshot configtest
+# # simulate first run
+# rsnapshot -t hourly
+# # now need to  sudo rsnapshot hourly  and monitor progress with  du -sh rsnapshot
 
--- build-scripts/17-updates-tests.sh
+++ build-scripts/17-updates-tests.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-# vim: fdm=expr fdc=1 ft=shbuild:
-
-# bash build.sh
+# vim: fdm=expr fdc=1 fdl=1 ft=shbuild:
 
 set -v  # prints each statement here, including comments
 trap read debug  # puts a read request after each executable line
