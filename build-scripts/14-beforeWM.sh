4fba6e7823466bd9d96ead8f43d709da92e406f8
Joseph Harriott <trohib@gmail.com>
2020-08-06 13:57:40 +0200

    test rsnapshot conf

--- build-scripts/14-beforeWM.sh
+++ build-scripts/14-beforeWM.sh
@@ -7,20 +7,27 @@ trap read debug  # puts a read request after each executable line
 ARCHBUILDS=~/ArchBuilds
     MACHINE=$ARCHBUILDS/sbMb
 
-#=> make AUR directory
-mkdir -p ~/Arch/AUR
+# #=> make AUR directory
+# mkdir -p ~/Arch/AUR
 
-#=> grab default rsnapshot conf
-cp /etc/rsnapshot.conf /mm/k86-rsnapshot.conf
+# #=> grab default rsnapshot conf
+# cp /etc/rsnapshot.conf /mm/k86-rsnapshot.conf
 
-#=> rsnapshot conf, for first run
+# #=> rsnapshot conf, for first run
 # my configuration
-mkdir /mnt/WD30EZRZ/rsnapshot
-sudo cp $MACHINE/etc/rsnapshot.conf /etc/rsnapshot.conf
+# mkdir /mnt/WD30EZRZ/rsnapshot
+# sudo cp $MACHINE/etc/rsnapshot.conf /etc/rsnapshot.conf
+
+#=> test rsnapshot conf
+# needed folders
+mkdir /mnt/SD480GSSDPlus/Dropbox
+mkdir /mnt/9QF57J6Q/Share
 
 # check
 rsnapshot configtest
+
 # test
 rsnapshot -t hourly
+
 # now can  sudo rsnapshot hourly  and monitor progress with  du -sh rsnapshot
 
