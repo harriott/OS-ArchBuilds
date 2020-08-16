5ce1c7ad8c6e7638bcceec0983b1649541a9bf3d
Joseph Harriott <trohib@gmail.com>
2020-08-16 20:44:13 +0200

    some symlinks

--- build-scripts/16-symlinks-jo.sh
+++ build-scripts/16-symlinks-jo.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 # vim: fdm=expr fdc=1 fdl=1 ft=shbuild:
 
-MACHINE=$ARCHBUILDS/sbMb
+#=> grab default xinitrc
+# need a removable drive mounted to /mm
+cp /etc/X11/xinit/xinitrc /mm/k8g-xinitrc
+echo "build your xinitrc"
 
+#=> some symlinks
 # sort this list by last use of "~":  :sort /,*\~/
 
 ln -sf $ARCHBUILDS/jo/Vim/init.vim    ~/.config/nvim/init.vim
 ln -sf $ARCHBUILDS/jo/Vim/gvimrc      ~/.gvimrc
 ln -sf $ARCHBUILDS/jo/Vim/vimrc       ~/.vimrc
-ln -sf $MACHINE/jo/xinitrc            ~/.xinitrc
 ln -sf $ARCHBUILDS/jo/bash_history.sh ~/Arch/bash_history.sh
 
---
+++ build-scripts/17-whenDropbox-xinitrc.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# vim: ft=shbuild:
+
+MACHINE=$ARCHBUILDS/sbMb
+
+ln -sf $MACHINE/jo/xinitrc ~/.xinitrc
+
