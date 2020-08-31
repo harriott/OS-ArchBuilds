29a14dc54c7d10a93d5015bfbab791580c2d1668
Joseph Harriott <trohib@gmail.com>
2020-08-31 22:58:31 +0200

    MACHINE/jo

--- build-scripts/21-Openbox-symlinks.sh
+++ build-scripts/21-Openbox-symlinks.sh
@@ -15,7 +15,7 @@ ln -sf $MACHINE/jo/openbox/schema.pl  ~/.config/obmenu-generator/schema.pl
 ln -sf $MACHINE/jo/openbox/autostart  ~/.config/openbox/autostart
 ln -sf $Openbox/openbox/menu.xml      ~/.config/openbox/menu.xml
 ln -sf $Openbox/openbox/rc.xml        ~/.config/openbox/rc.xml
-ln -sf $Openbox/Xresources/Xresources ~/.Xresources
+ln -sf $MACHINE/jo/Xresources         ~/.Xresources
 
 #=> 1 xinitrc
 ln -sf $MACHINE/jo/xinitrc ~/.xinitrc
