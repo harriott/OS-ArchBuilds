fa014e68bbff221b19b192d0a1ba073209d1b665
Joseph Harriott <trohib@gmail.com>
2020-08-31 18:00:38 +0200

    [[

--- build-scripts/19-gitconfig.sh
+++ build-scripts/19-gitconfig.sh
@@ -3,7 +3,7 @@
 
 #=> gitconfig
 rm ~/.gitconfig
-if [ -d $DROPBOX ]; then # assume that my Dropbox is up
+if [[ -d $DROPBOX ]]; then # assume that my Dropbox is up
     ln -sf $ARCHBUILDS/jo/gitconfig ~/.gitconfig
 else # use my  $ITstack/gitconfig  from a removable drive
     cp /mm/gitconfig ~/.gitconfig
