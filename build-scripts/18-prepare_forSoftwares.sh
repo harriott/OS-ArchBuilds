967ee79cde03c2bbfff13abfc09015c06e92e06a
Joseph Harriott <trohib@gmail.com>
2020-09-01 10:57:10 +0200

    Dropbox public key

--- build-scripts/18-prepare_forSoftwares.sh
+++ build-scripts/18-prepare_forSoftwares.sh
@@ -6,12 +6,22 @@
 set -v  # prints each statement here, including comments
 trap read debug  # puts a read request after each executable line
 
+#=> 0 Dropbox public key
+gpg --recv-keys 1C61A2656FB57B7E4DE0F4C1FC918B335044912E
+
 # #=> 0 GnuPG directory
 # # create & populate ~/.gnupg
 # gpg --list-keys
 
-#=> 0 Pandoc directory
-mkdir -p ~/.pandoc/defaults
+# #=> 0 Pandoc directory
+# mkdir -p ~/.pandoc/defaults
+
+#=> 0 Rust Toolchain - for Dust
+# 1 rustup
+sudo pacman -S rustup
+
+# 2 Rust Toolchain
+rustup toolchain install stable
 
 # #=> 1 configure GnuPG for pinentry
 # # gpg-agent.conf symlink for passphrase
--- build-scripts/toRenumber/15-whenWM-softwares.sh
+++ build-scripts/toRenumber/15-whenWM-softwares.sh
@@ -6,21 +6,7 @@
 set -v  # prints each statement here, including comments
 trap read debug  # puts a read request after each executable line
 
-# #=> AUR 0 preparations
-
-# # #==> Dropbox public key
-# # gpg --recv-keys 1C61A2656FB57B7E4DE0F4C1FC918B335044912E
-
-# # #==> Dust - Rust Toolchain
-# # # 1 rustup
-# # sudo pacman -S rustup
-
-# # # 2 Rust Toolchain
-# # rustup toolchain install stable
-
-# #=> AUR 1 AURs
-
-# #==> 8192eu-dkms - install
+# #=> 8192eu 0 8192eu-dkms - install
 # # needs  appmenu-gtk-module dkms linux-headers
 # cd ~/Arch/AUR
 # rm -fr 8192eu-dkms # (-f is needed for some weird files therein)
@@ -32,12 +18,15 @@ trap read debug  # puts a read request after each executable line
 # # reboot !
 # true
 
-# #==> 8192eu-dkms - remove
+# #=> 8192eu 1 blacklist rtl8xxxu
+# echo "blacklist rtl8xxxu" | sudo tee /etc/modprobe.d/rtl8xxxu.conf
+
+# #=> 8192eu 2 8192eu-dkms - remove
 # sudo pacman -Rs 8192eu-dkms
 # reboot !
 # true
 
-# #==> auracle-git
+# #=> auracle-git
 # cd ~/Arch/AUR
 # sudo rm -r auracle-git
 # git clone https://aur.archlinux.org/auracle-git.git
@@ -46,7 +35,7 @@ trap read debug  # puts a read request after each executable line
 # makepkg -sic
 # cd ~
 
-# #==> bat-extras-git
+# #=> bat-extras-git
 # cd ~/Arch/AUR
 # sudo rm -r bat-extras-git
 # git clone https://aur.archlinux.org/bat-extras-git.git
@@ -55,7 +44,7 @@ trap read debug  # puts a read request after each executable line
 # makepkg -sic
 # cd ~
 
-# #==> chromium-widevine
+# #=> chromium-widevine
 # cd ~/Arch/AUR
 # sudo rm -r chromium-widevine
 # git clone https://aur.archlinux.org/chromium-widevine.git
@@ -64,7 +53,7 @@ trap read debug  # puts a read request after each executable line
 # makepkg -sic
 # cd ~
 
-# #==> colorpicker
+# #=> colorpicker
 # cd ~/Arch/AUR
 # git clone https://aur.archlinux.org/colorpicker.git
 # cd colorpicker
@@ -72,17 +61,17 @@ trap read debug  # puts a read request after each executable line
 # makepkg -sic
 # cd ~
 
-# #==> Dropbox
-# # only once there's a browser, and preferably after LastPass
-# cd ~/Arch/AUR
-# sudo rm -r dropbox
-# git clone https://aur.archlinux.org/dropbox.git
-# cd dropbox
-# xdg-open PKGBUILD
-# makepkg -sic
-# cd ~
+#=> Dropbox
+# only once there's a browser, and preferably after LastPass
+cd ~/Arch/AUR
+sudo rm -r dropbox
+git clone https://aur.archlinux.org/dropbox.git
+cd dropbox
+xdg-open PKGBUILD
+makepkg -sic
+cd ~
 
-# #==> Dust
+# #=> Dust
 # # (after Rust Toolchain)
 # cd ~/Arch/AUR
 # sudo rm -r dust
@@ -92,7 +81,7 @@ trap read debug  # puts a read request after each executable line
 # makepkg -sic
 # cd ~
 
-# #==> Fangfrisch
+# #=> Fangfrisch 1 AUR
 # cd ~/Arch/AUR
 # git clone https://aur.archlinux.org/python-fangfrisch.git
 # cd python-fangfrisch
@@ -100,7 +89,17 @@ trap read debug  # puts a read request after each executable line
 # makepkg -sic
 # cd ~
 
-# #==> figlet-fonts
+# #=> Fangfrisch 2 settings
+# # 1 create database structure
+# sudo -u clamav /usr/bin/fangfrisch --conf /etc/fangfrisch/fangfrisch.conf initdb
+
+# # 2 enable systemd timer
+# sudo systemctl enable fangfrisch.timer
+
+# #=> ffmpeg
+# sudo pacman -S ffmpeg
+
+# #=> figlet-fonts
 # cd ~/Arch/AUR
 # git clone https://aur.archlinux.org/figlet-fonts.git
 # cd figlet-fonts
@@ -108,7 +107,10 @@ trap read debug  # puts a read request after each executable line
 # makepkg -sic
 # cd ~
 
-# #==> ffmpeg-gl-transition
+# #=> Firefox
+# sudo pacman -S firefox
+
+# #=> ffmpeg-gl-transition
 # cd ~/Arch/AUR
 # git clone https://aur.archlinux.org/ffmpeg-gl-transition.git
 # cd ffmpeg-gl-transition
@@ -116,7 +118,18 @@ trap read debug  # puts a read request after each executable line
 # makepkg -sic
 # cd ~
 
-# #==> Mapscii
+# #=> gifski
+# sudo pacman -S gifski
+
+# #=> gucharmap
+# sudo pacman -S gucharmap
+# # Accessories > Character Map > View > By Unicode Block
+
+# #=> java-commons-lang
+# # for pdftk's functions
+# sudo pacman -S java-commons-lang
+
+# #=> Mapscii
 # cd ~/Arch/AUR
 # sudo rm -r nodejs-mapscii
 # git clone https://aur.archlinux.org/nodejs-mapscii.git
@@ -127,7 +140,7 @@ trap read debug  # puts a read request after each executable line
 
 # sudo pacman -Rs nodejs-mapscii
 
-# #==> moar
+# #=> moar
 # cd ~/Arch/AUR
 # git clone https://aur.archlinux.org/moar.git
 # sudo rm -r moar
@@ -136,162 +149,13 @@ trap read debug  # puts a read request after each executable line
 # makepkg -sic
 # cd ~
 
-# #==> nodejs-ffmpeg-concat
+# #=> nodejs-ffmpeg-concat
 # cd ~/Arch/AUR
 # git clone https://aur.archlinux.org/nodejs-ffmpeg-concat.git
 # cd nodejs-ffmpeg-concat
 # xdg-open PKGBUILD
 # makepkg -sic
 # cd ~
-
-# #==> perl-clone-pp
-# cd ~/Arch/AUR
-# sudo rm -r perl-clone-pp
-# git clone https://aur.archlinux.org/perl-clone-pp.git
-# cd perl-clone-pp
-# xdg-open PKGBUILD
-# makepkg -sic
-# cd ~
-
-# #==> perl-data-printer
-# cd ~/Arch/AUR
-# sudo rm -r perl-data-printer
-# git clone https://aur.archlinux.org/perl-data-printer.git
-# cd perl-data-printer
-# xdg-open PKGBUILD
-# makepkg -sic
-# cd ~
-
-# #==> Pinta
-# cd ~/Arch/AUR
-# git clone https://aur.archlinux.org/pinta.git
-# cd pinta
-# xdg-open PKGBUILD
-# makepkg -sic
-# cd ~
-
-# #==> pscircle-git
-# cd ~/Arch/AUR
-# git clone https://aur.archlinux.org/pscircle-git.git
-# cd pscircle-git
-# xdg-open PKGBUILD
-# makepkg -sic
-# cd ~
-
-# #==> quickviewer
-# cd ~/Arch/AUR
-# git clone https://aur.archlinux.org/quickviewer.git
-# cd quickviewer
-# xdg-open PKGBUILD
-# makepkg -sic
-# cd ~
-
-# #==> rtl8192eu
-# cd ~/Arch/AUR
-# sudo rm -r rtl8192eu
-# git clone https://aur.archlinux.org/rtl8192eu.git
-# cd rtl8192eu
-# xdg-open PKGBUILD
-# makepkg -sic
-# cd ~
-
-# #==> Samokovarov's jump
-# cd ~/Arch/AUR
-# git clone https://aur.archlinux.org/jump.git
-# cd jump
-# xdg-open PKGBUILD
-# makepkg -sic
-# cd ~
-
-# #==> vim Calendar
-# cd ~/Arch/AUR
-# git clone https://aur.archlinux.org/vim-calendar-git.git
-# cd vim-calendar-git
-# xdg-open PKGBUILD
-# makepkg -sic
-# cd ~
-
-# #==> vim LanguageTool
-# cd ~/Arch/AUR
-# git clone https://aur.archlinux.org/vim-languagetool.git
-# cd vim-languagetool
-# xdg-open PKGBUILD
-# makepkg -sic
-# cd ~
-
-# #==> vim-mediawiki
-# cd ~/Arch/AUR
-# git clone https://aur.archlinux.org/vim-mediawiki.git
-# cd vim-mediawiki
-# xdg-open PKGBUILD
-# makepkg -sic
-# cd ~
-
-# #==> vim-simpylfold
-# cd ~/Arch/AUR
-# git clone https://aur.archlinux.org/vim-simpylfold.git
-# cd vim-simpylfold
-# xdg-open PKGBUILD
-# makepkg -sic
-# cd ~
-
-# #==> vim Solarized
-# cd ~/Arch/AUR
-# git clone https://aur.archlinux.org/vim-colors-solarized-git.git
-# cd vim-colors-solarized-git
-# xdg-open PKGBUILD
-# makepkg -sic
-# cd ~
-
-# #==> vim-undotree
-# cd ~/Arch/AUR
-# sudo rm -r vim-undotree
-# git clone https://aur.archlinux.org/vim-undotree.git
-# cd vim-undotree
-# xdg-open PKGBUILD
-# makepkg -sic
-# cd ~
-
-# #==> xbindkeys
-# sudo pacman -S xbindkeys
-
-# #==> Zoom
-# cd ~/Arch/AUR
-# sudo rm -r zoom
-# git clone https://aur.archlinux.org/zoom.git
-# cd zoom
-# xdg-open PKGBUILD
-# makepkg -sic
-# cd ~
-
-# #=> AUR 2 settings
-
-# #==> 8192eu - blacklist rtl8xxxu
-# echo "blacklist rtl8xxxu" | sudo tee /etc/modprobe.d/rtl8xxxu.conf
-
-# #==> Fangfrisch 2 settings
-# # 1 create database structure
-# sudo -u clamav /usr/bin/fangfrisch --conf /etc/fangfrisch/fangfrisch.conf initdb
-
-# # 2 enable systemd timer
-# sudo systemctl enable fangfrisch.timer
-
-# #=> ffmpeg
-# sudo pacman -S ffmpeg
-
-# #=> Firefox
-# sudo pacman -S firefox
-
-# #=> gifski
-# sudo pacman -S gifski
-
-# #=> gucharmap
-# sudo pacman -S gucharmap
-# # Accessories > Character Map > View > By Unicode Block
-
-#=> java-commons-lang
-# for pdftk's functions
-sudo pacman -S java-commons-lang
 
 # #=> npm ffmpeg-concat
 # sudo pacman -S npm
@@ -312,4 +176,124 @@ sudo pacman -S java-commons-lang
 
 # #=> Perl modules for JSON
 # sudo pacman -S perl-json perl-json-parse perl-json-xs
+
+# #=> perl-clone-pp
+# cd ~/Arch/AUR
+# sudo rm -r perl-clone-pp
+# git clone https://aur.archlinux.org/perl-clone-pp.git
+# cd perl-clone-pp
+# xdg-open PKGBUILD
+# makepkg -sic
+# cd ~
+
+# #=> perl-data-printer
+# cd ~/Arch/AUR
+# sudo rm -r perl-data-printer
+# git clone https://aur.archlinux.org/perl-data-printer.git
+# cd perl-data-printer
+# xdg-open PKGBUILD
+# makepkg -sic
+# cd ~
+
+# #=> Pinta
+# cd ~/Arch/AUR
+# git clone https://aur.archlinux.org/pinta.git
+# cd pinta
+# xdg-open PKGBUILD
+# makepkg -sic
+# cd ~
+
+# #=> pscircle-git
+# cd ~/Arch/AUR
+# git clone https://aur.archlinux.org/pscircle-git.git
+# cd pscircle-git
+# xdg-open PKGBUILD
+# makepkg -sic
+# cd ~
+
+# #=> quickviewer
+# cd ~/Arch/AUR
+# git clone https://aur.archlinux.org/quickviewer.git
+# cd quickviewer
+# xdg-open PKGBUILD
+# makepkg -sic
+# cd ~
+
+# #=> rtl8192eu
+# cd ~/Arch/AUR
+# sudo rm -r rtl8192eu
+# git clone https://aur.archlinux.org/rtl8192eu.git
+# cd rtl8192eu
+# xdg-open PKGBUILD
+# makepkg -sic
+# cd ~
+
+# #=> Samokovarov's jump
+# cd ~/Arch/AUR
+# git clone https://aur.archlinux.org/jump.git
+# cd jump
+# xdg-open PKGBUILD
+# makepkg -sic
+# cd ~
+
+# #=> vim Calendar
+# cd ~/Arch/AUR
+# git clone https://aur.archlinux.org/vim-calendar-git.git
+# cd vim-calendar-git
+# xdg-open PKGBUILD
+# makepkg -sic
+# cd ~
+
+# #=> vim LanguageTool
+# cd ~/Arch/AUR
+# git clone https://aur.archlinux.org/vim-languagetool.git
+# cd vim-languagetool
+# xdg-open PKGBUILD
+# makepkg -sic
+# cd ~
+
+# #=> vim-mediawiki
+# cd ~/Arch/AUR
+# git clone https://aur.archlinux.org/vim-mediawiki.git
+# cd vim-mediawiki
+# xdg-open PKGBUILD
+# makepkg -sic
+# cd ~
+
+# #=> vim-simpylfold
+# cd ~/Arch/AUR
+# git clone https://aur.archlinux.org/vim-simpylfold.git
+# cd vim-simpylfold
+# xdg-open PKGBUILD
+# makepkg -sic
+# cd ~
+
+# #=> vim Solarized
+# cd ~/Arch/AUR
+# git clone https://aur.archlinux.org/vim-colors-solarized-git.git
+# cd vim-colors-solarized-git
+# xdg-open PKGBUILD
+# makepkg -sic
+# cd ~
+
+# #=> vim-undotree
+# cd ~/Arch/AUR
+# sudo rm -r vim-undotree
+# git clone https://aur.archlinux.org/vim-undotree.git
+# cd vim-undotree
+# xdg-open PKGBUILD
+# makepkg -sic
+# cd ~
+
+# #=> xbindkeys
+# sudo pacman -S xbindkeys
+
+# #=> Zoom
+# cd ~/Arch/AUR
+# sudo rm -r zoom
+# git clone https://aur.archlinux.org/zoom.git
+# cd zoom
+# xdg-open PKGBUILD
+# makepkg -sic
+# cd ~
 
