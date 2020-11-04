37b1b680533ab781c310d7c6a0a6c2f7ff762dba
Joseph Harriott <trohib@gmail.com>
2020-11-04 22:04:12 +0100

    ~/.config/zathura

--- build-scripts/23-softwaresWhenOpenbox.sh
+++ build-scripts/23-softwaresWhenOpenbox.sh
@@ -16,68 +16,72 @@ trap read debug  # puts a read request after each executable line
 # makepkg -sic
 # cd ~
 
-#=> 1 gphoto2
-sudo pacman -S gphoto2
+# #=> 1 gphoto2
+# sudo pacman -S gphoto2
 
-#=> 1 mpv
-sudo pacman -S mpv
+# #=> 1 mpv
+# sudo pacman -S mpv
 
-#=> 1 nomacs
-sudo pacman -S nomacs
-# now run it for  Ctrl+Shift+p > Close on ESC
+# #=> 1 nomacs
+# sudo pacman -S nomacs
+# # now run it for  English  then  Close and run it again for  Ctrl+Shift+p > Close on ESC
+# true
 
-#=> 1 PCManFM
-sudo pacman -S pcmanfm
-# now run it to set some of my preferences:  Edit > Preferences >
-#  Terminal emulator:  Advanced > Terminal > urxvt
-#  Trash: Confirm before moving files into "trash can"   off-ticked
-#  Volume Management > Show available options  off-ticked
+# #=> 1 PCManFM
+# sudo pacman -S pcmanfm
+# handlr set inode/directory pcmanfm.desktop
+# # now run it to set some of my preferences:  Edit > Preferences >
+# #  Advanced > Terminal > urxvt
+# #  General > Confirm before moving files into "trash can"   off-ticked
+# #  Volume Management > Show available options  off-ticked
+# true
 
-#=> 1 Pinta
-sudo pacman -S pinta
+# #=> 1 Pinta
+# sudo pacman -S pinta
 
-#=> 1 PNMixer
-cd ~/Arch/AUR
-git clone https://aur.archlinux.org/pnmixer.git
-cd pnmixer
-gvim PKGBUILD
-makepkg -sic
-cd ~
-# now  Multimedia > PNMixer > Preferences > View > Draw Volume Meter on Tray Icon
+# #=> 1 PNMixer
+# cd ~/Arch/AUR
+# git clone https://aur.archlinux.org/pnmixer.git
+# cd pnmixer
+# gvim PKGBUILD
+# makepkg -sic
+# cd ~
+# # now  Multimedia > PNMixer > Preferences > View > Draw Volume Meter on Tray Icon
+# true
 
-#=> 1 qt5-styleplugins
-# for qt5ct - allows fix of narrow fonts in XnViewMP
-cd ~/Arch/AUR
-sudo rm -r qt5-styleplugins
-git clone https://aur.archlinux.org/qt5-styleplugins.git
-cd qt5-styleplugins
-gvim PKGBUILD
-makepkg -sic
-cd ~
+# #=> 1 qt5-styleplugins
+# # for qt5ct - allows fix of narrow fonts in XnViewMP
+# cd ~/Arch/AUR
+# sudo rm -r qt5-styleplugins
+# git clone https://aur.archlinux.org/qt5-styleplugins.git
+# cd qt5-styleplugins
+# gvim PKGBUILD
+# makepkg -sic
+# cd ~
 
-#=> 1 qpdfview
-sudo pacman -S qpdfview
+# #=> 1 qpdfview
+# sudo pacman -S qpdfview
 
-#=> 1 qt5ct
-# - Qt5 theme - allows better tab differentiation in CopyQ
-sudo pacman -S qt5ct
-grep QT_QPA_PLATFORMTHEME=qt5ct ~/.xinitrc
-# now need to  Menu > Settings > Qt5 Settings > Style > cleanlooks
-sed -i 's/fusion/cleanlooks/' ~/.config/qt5ct/qt5ct.conf
+# #=> 1 qt5ct
+# # - Qt5 theme - allows better tab differentiation in CopyQ
+# sudo pacman -S qt5ct
+# #  QT_QPA_PLATFORMTHEME=qt5ct  is set in  ~/.xinitrc
+# sed -i 's/fusion/cleanlooks/' ~/.config/qt5ct/qt5ct.conf
+# #  = Menu > Settings > Qt5 Settings > Style > cleanlooks
 
-#=> 1 SANE
-sudo pacman -S sane
+# #=> 1 SANE
+# sudo pacman -S sane
 
-#=> 1 sxiv
-sudo pacman -S sxiv
+# #=> 1 sxiv
+# sudo pacman -S sxiv
 
-#=> 1 tmux-bash-completion-git
-cd ~/Arch/AURdev-clone
-git clone https://aur.archlinux.org/tmux-bash-completion-git.git
-gctm tmux-bash-completion-git
-gvim PKGBUILD
-makepkg -sic
-cd ~
+# #=> 1 tmux-bash-completion-git
+# cd ~/Arch/AURdev-clone
+# git clone https://aur.archlinux.org/tmux-bash-completion-git.git
+# gctm tmux-bash-completion-git
+# gvim PKGBUILD
+# makepkg -sic
+# cd ~
 
 # #=> 1 urlview
 # # creates a numbered list of url's in a resource
@@ -90,5 +94,6 @@ cd ~
 
 #=> 1 Zathura
 sudo pacman -S zathura-pdf-mupdf
+mkdir ~/.config/zathura
 ln -fs $Openbox/zathurarc ~/.config/zathura/zathurarc
 
