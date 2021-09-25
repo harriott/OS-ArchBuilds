8d30457643d8c9d2385cdec5d566e96c86144dac
Joseph Harriott <trohib@gmail.com>
2021-09-25 13:11:34 +0200

    update /ArchBuilds

--- build-scripts/03-cloneArchBuilds.sh
+++ build-scripts/03-cloneArchBuilds.sh
@@ -4,7 +4,6 @@
 # cd /; bash ArchBuilds/build-scripts/03-cloneArchBuilds.sh; cd ArchBuilds/build-scripts
 
 #=> 0 grab latest ArchBuilds
-cd /
 [ -d ArchBuilds ] && mv ArchBuilds ArchBuildsOld  # because my config specifies ssh
 git clone https://github.com/harriott/ArchBuilds.git
 ls /ArchBuilds
