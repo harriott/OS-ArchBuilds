55aea69c766c99062b9f36474a4eb68e5a257701
Joseph Harriott <trohib@gmail.com>
2021-10-05 14:17:11 +0200

    get misc

---
+++ build-scripts/18-misc.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# vim: set fdl=1 sw=2:
+
+#=> get misc
+cd ~
+
+#==> 0 HTTPS
+git clone https://github.com/harriott/misc.git
+
+# #==> 1 SSH
+# git clone git@github.com:harriott/misc.git  # .git/config > %s//gh/
+
---
+++ build-scripts/19-git_ssh.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# vim: set fdl=1 sw=2:
+
+set -v  # prints each statement here, including comments
+trap read debug  # puts a read request after each executable line
+
+#=> 1 first SSH to GitHub
+ssh -T git@github.com  # "yes"
+cat ~/.ssh/known_hosts
+cp ~/.ssh/known_hosts ~/known_hosts-la5
+
