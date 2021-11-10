1eb1726a2bbef298b6d190f17999ee6d208cc909
Joseph Harriott <trohib@gmail.com>
2021-11-10 22:22:58 +0100

    tweak GRUB

--- build-scripts/28-Zen.sh
+++ build-scripts/28-Zen.sh
@@ -6,10 +6,18 @@
 set -v  # prints each statement here, including comments
 trap read debug  # puts a read request after each executable line
 
-# #=> 1 Zen
+# #=> 0 Zen
 # sudo pacman -S linux-zen
+
+#=> 1 tweak GRUB
+sudo sed -i 's/^GRUB_DEFAULT=0/GRUB_DEFAULT=saved/' /etc/default/grub
+rg '^GRUB_DEFAULT' /etc/default/grub
+sudo sed -i 's/#GRUB_SAVEDEFAULT/GRUB_SAVEDEFAULT/' /etc/default/grub
+rg GRUB_SAVEDEFAULT /etc/default/grub
+sudo sed -i 's/#GRUB_DISABLE_SUBMENU/GRUB_DISABLE_SUBMENU/' /etc/default/grub
+rg GRUB_DISABLE_SUBMENU /etc/default/grub
 
 #=> 1 update GRUB
 sudo grub-mkconfig -o /boot/grub/grub.cfg
-# now  reboot  for  GRUB > Advanced optons for Arch Linux > Arch Linux, with Linux linux
+# now  reboot  for  GNU GRUB > Advanced optons for Arch Linux > Arch Linux, with Linux linux-zen
 
