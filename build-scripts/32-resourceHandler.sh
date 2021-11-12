69788523a34fee0504c2bbfb246e71db5b258439
Joseph Harriott <trohib@gmail.com>
2021-11-12 14:52:49 +0100

    remove xdg-utils

--- build-scripts/32-resourceHandler.sh
+++ build-scripts/32-resourceHandler.sh
@@ -5,19 +5,27 @@
 set -v  # prints each statement here, including comments
 trap read debug  # puts a read request after each executable line
 
-#=> 0 handlr-bin
-gAUR handlr-bin
-nvim -c "silent! /handlr" PKGBUILD
-makepkg -sic
-cd ~
+#=> handlr
 
-#=> 1 xdg-utils-handlr
+# #==> 0 handlr-bin
+# gAUR handlr-bin
+# nvim -c "silent! /handlr" PKGBUILD
+# makepkg -sic
+# cd ~
+
+#==> 1 remove xdg-utils
+# because in conflict with xdg-utils-handlr
+sudo pacman -Rs xdg-utils
+
+#==> 2 xdg-utils-handlr
 gAUR xdg-utils-handlr
 nvim -c "silent! /handlr" PKGBUILD
 makepkg -sic
 cd ~
 
-# #=> 0 mimeo
+# #=> mimeo
+
+# #==> 0 mimeo
 # cd ~/Arch/AUR
 # git clone https://aur.archlinux.org/mimeo.git
 # cd mimeo
@@ -25,7 +33,7 @@ cd ~
 # makepkg -sic
 # cd ~
 
-# #=> 1 xdg-utils-mimeo
+# #==> 1 xdg-utils-mimeo
 # cd ~/Arch/AUR
 # git clone https://aur.archlinux.org/xdg-utils-mimeo.git
 # cd xdg-utils-mimeo
