133e280be9dc5ce234fa84cb489cd0d358fe10ca
Joseph Harriott <trohib@gmail.com>
2021-11-14 17:47:41 +0100

    "~/Arch/AUR$2"

--- build-scripts/34-softwares-forOpenbox.sh
+++ build-scripts/34-softwares-forOpenbox.sh
@@ -23,52 +23,46 @@ trap read debug  # puts a read request after each executable line
 # #=> 1 mpv
 # sudo pacman -S mpv
 
-#=> 1 nomacs
-sudo pacman -S nomacs
-# now run it for  English  then  Close and run it again for  Ctrl+Shift+p > Close on ESC
-true
+# #=> 1 nomacs
+# sudo pacman -S nomacs
+# # now run it for  English  then  Close and run it again for  Ctrl+Shift+p > Close on ESC
+# true
 
-#=> 1 PCManFM
-sudo pacman -S pcmanfm
-handlr set inode/directory pcmanfm.desktop
-# now run it to set some of my preferences:  Edit > Preferences >
-#  Advanced > Terminal > urxvt
-#  General > Confirm before moving files into "trash can"   off-ticked
-#  Volume Management > Show available options  off-ticked
-true
+# #=> 1 PCManFM
+# sudo pacman -S pcmanfm
+# handlr set inode/directory pcmanfm.desktop
+# # now run it to set some of my preferences:  Edit > Preferences >
+# #  General > Confirm before moving files into "trash can"   off-ticked
+# #  Volume Management > Show available options  off-ticked
+# #  Advanced > Terminal > urxvt
+# true
 
-#=> 1 Pinta
-sudo pacman -S pinta
+# #=> 1 Pinta
+# sudo pacman -S pinta
 
-#=> 1 PNMixer
-cd ~/Arch/AUR
-git clone https://aur.archlinux.org/pnmixer.git
-cd pnmixer
-gvim PKGBUILD
-makepkg -sic
-cd ~
-# now  Multimedia > PNMixer > Preferences > View > Draw Volume Meter on Tray Icon
-true
+# #=> 1 PNMixer
+# gAUR pnmixer
+# gvim -c "silent! /pnmixer-v" PKGBUILD
+# makepkg -sic
+# # now  Multimedia > PNMixer > Preferences > View > Draw Volume Meter on Tray Icon
+# true
 
-#=> 1 qt5-styleplugins
-# for qt5ct - allows fix of narrow fonts in XnViewMP
-gAUR qt5-styleplugins
-gvim PKGBUILD
-makepkg -sic
-cd ~
+# #=> 1 qt5-styleplugins
+# # for qt5ct - allows fix of narrow fonts in XnViewMP
+# gAUR qt5-styleplugins
+# gvim -c "silent! /qtstyleplugins" PKGBUILD
+# makepkg -sic  # takes a long time
 
-#=> 1 qpdfview
-sudo pacman -S qpdfview
+# #=> 1 qpdfview
+# sudo pacman -S qpdfview
 
-#=> 1 qt5ct
-# - Qt5 theme - allows better tab differentiation in CopyQ
-sudo pacman -S qt5ct
-#  QT_QPA_PLATFORMTHEME=qt5ct  is set in  ~/.xinitrc
-sed -i 's/fusion/cleanlooks/' ~/.config/qt5ct/qt5ct.conf
-#  = Menu > Settings > Qt5 Settings > Style > cleanlooks
+# #=> 1 qt5ct
+# # - Qt5 theme - allows better tab differentiation in CopyQ
+# sudo pacman -S qt5ct
+# #  QT_QPA_PLATFORMTHEME=qt5ct  is set in  ~/.xinitrc
 
-#=> 1 SANE
-sudo pacman -S sane
+# #=> 1 SANE
+# sudo pacman -S sane
 
 #=> 1 snixembed-git
 # to get Dropbox icon down into tint2
