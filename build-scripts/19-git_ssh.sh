a987e89b13bccc4408636dcea47fc9764fc89d8a
Joseph Harriott <trohib@gmail.com>
2021-11-14 15:52:11 +0100

    disable MAC address randomization

--- build-scripts/19-git_ssh.sh
+++ build-scripts/19-git_ssh.sh
@@ -6,12 +6,15 @@
 set -v  # prints each statement here, including comments
 trap read debug  # puts a read request after each executable line
 
-#=> 0 check configuration
-git config -l
+# #=> 0 git 0 check configuration
+# git config -l
 
-#=> 1 first SSH to GitHub
-# should see:  RSA key fingerprint is SHA256:nThbg6kXUpJWGl7E1IGOCspRomTxdCARLviKw6E5SY8.
-ssh -T git@github.com  # "yes"
-cat ~/.ssh/known_hosts
-cp ~/.ssh/known_hosts ~/known_hosts-la5
+# #=> 0 git 1 first SSH to GitHub
+# # should see:  RSA key fingerprint is SHA256:nThbg6kXUpJWGl7E1IGOCspRomTxdCARLviKw6E5SY8.
+# ssh -T git@github.com  # "yes"
+# cat ~/.ssh/known_hosts
+# cp ~/.ssh/known_hosts ~/known_hosts-la5
+
+#=> 0 NetworkManager - disable MAC address randomization
+cp $MACHINE/etc/wifi_rand_mac.conf /etc/NetworkManager/conf.d/wifi_rand_mac.conf
 
