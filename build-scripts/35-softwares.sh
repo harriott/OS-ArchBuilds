c7061df799c393a1f1aed1dbbd0d95fadbad165e
Joseph Harriott <trohib@gmail.com>
2021-11-15 13:10:23 +0100

    Spotify Key

--- build-scripts/35-softwares.sh
+++ build-scripts/35-softwares.sh
@@ -73,118 +73,118 @@ trap read debug  # puts a read request after each executable line
 # #=> Dropbox 1 dropbox-dist folder
 # install -dm0 ~/.dropbox-dist  # prevents automatic updates, allowing Arch installation to work
 
-#=> Dust
-# (after Rust Toolchain)
-sudo pacman -S dust
+# #=> Dust
+# # (after Rust Toolchain)
+# sudo pacman -S dust
 
-#=> ffmpeg
-sudo pacman -S ffmpeg
+# #=> ffmpeg
+# sudo pacman -S ffmpeg
 
-#=> fonts - figlet-fonts
-# brings in figlet
-gAUR figlet-fonts
-gvim -c "silent! /ftp.figlet.org" PKGBUILD
-makepkg -sic
+# #=> fonts - figlet-fonts
+# # brings in figlet
+# gAUR figlet-fonts
+# gvim -c "silent! /ftp.figlet.org" PKGBUILD
+# makepkg -sic
 
-#=> fonts - fontpreview-git
-gAUR fontpreview-git
-gvim -c "silent! /sdushantha" PKGBUILD
-makepkg -sic
+# #=> fonts - fontpreview-git
+# gAUR fontpreview-git
+# gvim -c "silent! /sdushantha" PKGBUILD
+# makepkg -sic
 
-#=> fonts - gnu-free-fonts
-sudo pacman -S gnu-free-fonts
+# #=> fonts - gnu-free-fonts
+# sudo pacman -S gnu-free-fonts
 
-#=> fonts - lohit-fonts
-gAUR lohit-fonts
-gvim -c "silent! /_fhosted" PKGBUILD
-makepkg -sic
+# #=> fonts - lohit-fonts
+# gAUR lohit-fonts
+# gvim -c "silent! /_fhosted" PKGBUILD
+# makepkg -sic
 
-#=> fonts - ttf-dejavu
-sudo pacman -S ttf-dejavu
+# #=> fonts - ttf-dejavu
+# sudo pacman -S ttf-dejavu
 
-#=> Falkon
-sudo pacman -S falkon
+# #=> Falkon
+# sudo pacman -S falkon
 
-#=> Firefox
-sudo pacman -S firefox
+# #=> Firefox
+# sudo pacman -S firefox
 
-#=> fangfrisch 1 AUR
-gAUR python-fangfrisch
-gvim -c "silent! /rseichter" PKGBUILD
-makepkg -sic
-true
+# #=> fangfrisch 1 AUR
+# gAUR python-fangfrisch
+# gvim -c "silent! /rseichter" PKGBUILD
+# makepkg -sic
+# true
 
-#=> fangfrisch 2 settings
-# 1 create database structure
-sudo -u clamav /usr/bin/fangfrisch --conf /etc/fangfrisch/fangfrisch.conf initdb
+# #=> fangfrisch 2 settings
+# # 1 create database structure
+# sudo -u clamav /usr/bin/fangfrisch --conf /etc/fangfrisch/fangfrisch.conf initdb
 
-# 2 enable systemd timer
-sudo systemctl enable fangfrisch.timer --now
+# # 2 enable systemd timer
+# sudo systemctl enable fangfrisch.timer --now
 
-#=> gifski
-sudo pacman -S gifski  # (for making animated GIFs)
+# #=> gifski
+# sudo pacman -S gifski  # (for making animated GIFs)
 
-#=> Google Chrome
-gAUR google-chrome
-gvim -c "silent! /dl.google.com" PKGBUILD
-makepkg -sic
-true
+# #=> Google Chrome
+# gAUR google-chrome
+# gvim -c "silent! /dl.google.com" PKGBUILD
+# makepkg -sic
+# true
 
-#=> gnome-disk-utility
-sudo pacman -S gnome-disk-utility  # (for Menu > Accessories > Disks)
+# #=> gnome-disk-utility
+# sudo pacman -S gnome-disk-utility  # (for Menu > Accessories > Disks)
 
-#=> gst123
-gAUR gst123
-gvim -c "silent! /stefan" PKGBUILD
-makepkg -sic
-true
+# #=> gst123
+# gAUR gst123
+# gvim -c "silent! /stefan" PKGBUILD
+# makepkg -sic  # takes a while
+# true
 
 # #=> Google API python toolchain
 # sudo pacman -S python-google-api-python-client
 # sudo pacman -S python-google-auth-httplib2
 # sudo pacman -S python-google-auth-oauthlib
 
-#=> gucharmap
-sudo pacman -S gucharmap  # (for Accessories > Character Map > View > By Unicode Block)
+# #=> gucharmap
+# sudo pacman -S gucharmap  # (for Accessories > Character Map > View > By Unicode Block)
 
-#=> GraphicsMagick
-sudo pacman -S graphicsmagick
+# #=> GraphicsMagick
+# sudo pacman -S graphicsmagick
 
-#=> HPLIP
-sudo pacman -S hplip
+# #=> HPLIP
+# sudo pacman -S hplip
 
-#=> java-commons-lang
-sudo pacman -S java-commons-lang  # (for pdftk's functions)
+# #=> java-commons-lang
+# sudo pacman -S java-commons-lang  # (for pdftk's functions)
 
-#=> keepassxc
-sudo pacman -S keepassxc
+# #=> keepassxc
+# sudo pacman -S keepassxc
 
-#=> LanguageTool
-sudo pacman -S languagetool
+# #=> LanguageTool
+# sudo pacman -S languagetool
 
 # #=> libappindicator-gtk3
 # sudo pacman -S libappindicator-gtk3  # didn't help with non-captured Dropbox tray icon...
 
-#=> libgphoto2
-sudo pacman -S gphoto2  # brings in  libgphoto2  & gives cli access to it
-sudo pacman -S gvfs-gphoto2  # for GNOME Files integration
+# #=> libgphoto2
+# sudo pacman -S gphoto2  # brings in  libgphoto2  & gives cli access to it
+# sudo pacman -S gvfs-gphoto2  # for GNOME Files integration
 
-#=> libva-utils
-sudo pacman -S libva-utils
+# #=> libva-utils
+# sudo pacman -S libva-utils
 
-#=> mesa-demos
-sudo pacman -S mesa-demos
+# #=> mesa-demos
+# sudo pacman -S mesa-demos
 
 # #=> moar remove
 # cd ~/Arch/AUR
 # rAUR moar
 # sudo pacman -Rs moar
 
-#=> moar-git 0 install
-gAUR moar-git
-gvim -c "silent! /walles" PKGBUILD
-makepkg -sic
-true
+# #=> moar-git 0 install
+# gAUR moar-git
+# gvim -c "silent! /walles" PKGBUILD
+# makepkg -sic
+# true
 
 # #=> moar-git 1 remove
 # cd ~/Arch/AUR
@@ -207,17 +207,17 @@ true
 # #=> nodejs-mapscii 1 remove
 # sudo pacman -Rs nodejs-mapscii
 
-#=> nordvpn-bin 0 install
-gAUR nordvpn-bin
-gvim -c "silent! /https:\/\/repo.nordvpn.com\/deb\/nordvpn\/debian\/pool\/main\/nordvpn_" PKGBUILD
-makepkg -sic
-true
+# #=> nordvpn-bin 0 install
+# gAUR nordvpn-bin
+# gvim -c "silent! /https:\/\/repo.nordvpn.com\/deb\/nordvpn\/debian\/pool\/main\/nordvpn_" PKGBUILD
+# makepkg -sic
+# true
 
-#=> nordvpn-bin 1 configure
-sudo usermod -aG nordvpn jo  # need to re-login
-true
-sudo systemctl enable nordvpnd.service --now
-true
+# #=> nordvpn-bin 1 configure
+# sudo usermod -aG nordvpn jo  # need to re-login
+# true
+# sudo systemctl enable nordvpnd.service --now
+# true
 
 # #=> OBS Studio 0 install
 # sudo pacman -S obs-studio
@@ -226,68 +226,69 @@ true
 # sudo pacman -Rs obs-studio
 # rm -r ~/.config/obs-studio
 
-#=> OpenShot
-sudo pacman -S openshot
+# #=> OpenShot
+# sudo pacman -S openshot
 
-#=> PDFsam
-# depends on  jdk-openjdk
-gAUR pdfsam
-gvim -c "silent! /torakiki" PKGBUILD
-makepkg -sic
+# #=> PDFsam
+# gAUR pdfsam
+# gvim -c "silent! /torakiki" PKGBUILD
+# makepkg -sic  # choose  jdk-openjdk
 
-#=> pdftk
-sudo pacman -S pdftk
+# #=> pdftk
+# sudo pacman -S pdftk
 
-#=> peek
-sudo pacman -S peek  # (GIF Screen Recorder)
+# #=> peek
+# sudo pacman -S peek  # (GIF Screen Recorder)
 
-#=> Perl modules for JSON
-sudo pacman -S perl-json perl-json-parse perl-json-xs
+# #=> Perl modules for JSON
+# sudo pacman -S perl-json perl-json-parse perl-json-xs
 
-#=> perl-clone-pp
-# for  perl-data-printer
-gAUR perl-clone-pp
-gvim -c "silent! /NEILB" PKGBUILD
-makepkg -sic
-true
+# #=> perl-clone-pp
+# # for  perl-data-printer
+# gAUR perl-clone-pp
+# gvim -c "silent! /NEILB" PKGBUILD
+# makepkg -sic
+# true
 
-#=> perl-data-printer
-gAUR perl-data-printer
-gvim -c "silent! /GARU" PKGBUILD
-makepkg -sic
-true
+# #=> perl-data-printer
+# gAUR perl-data-printer
+# gvim -c "silent! /GARU" PKGBUILD
+# makepkg -sic
+# true
 
-#=> perl-image-exiftool
-sudo pacman -S perl-image-exiftool
+# #=> perl-image-exiftool
+# sudo pacman -S perl-image-exiftool
 
 # #=> PhotoCollage
 # gAUR photocollage
 # gvim PKGBUILD
 # makepkg -sic
 
-#=> Pinta
-sudo pacman -S pinta
+# #=> Pinta
+# sudo pacman -S pinta
 
-#=> pip
-sudo pacman -S python-pip
+# #=> pip
+# sudo pacman -S python-pip
 
-#=> pscircle-git
-gAUR pscircle-git
-gvim -c "silent! /mildlyparallel" PKGBUILD
-makepkg -sic
+# #=> pscircle-git
+# gAUR pscircle-git
+# gvim -c "silent! /mildlyparallel" PKGBUILD
+# makepkg -sic
 
-#=> Python 2
-sudo pacman -S python2  # needed for  oauth2.py
+# #=> Python 2
+# sudo pacman -S python2  # needed for  oauth2.py
 
-#=> Quod Libet
-sudo pacman -S quodlibet
+# #=> Quod Libet
+# sudo pacman -S quodlibet
 
-#=> Samokovarov's jump
-gAUR jump
-gvim -c "silent! /gsamokovarov" PKGBUILD
-makepkg -sic
+# #=> Samokovarov's jump
+# gAUR jump
+# gvim -c "silent! /gsamokovarov" PKGBUILD
+# makepkg -sic
 
 #=> Spotify
+curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | gpg --import -
+gpg -k 0D811D58
 gAUR spotify
 gvim -c "silent! /http:\/\/repository.spotify.com\/dists\/testing\/non-free\/binary-amd64\/Packages" PKGBUILD
 makepkg -sic
