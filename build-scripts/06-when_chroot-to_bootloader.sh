6448acd7ddbe9f9f41baee48d5f45333c2a7a042
Joseph Harriott <trohib@gmail.com>
2022-07-29 01:00:43 +0200

    AsusW202/export-machine

--- build-scripts/06-when_chroot-to_bootloader.sh
+++ build-scripts/06-when_chroot-to_bootloader.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-set -v  # prints each statement here, including comments
-trap read debug  # puts a read request after each executable line
-
 #=> 0 $ARCHBUILDS
 # check  $ARCHBUILDS/Bash/export-storage  has leveraged  /ArchBuilds
 echo "\$ARCHBUILDS is $ARCHBUILDS"
 read -p "- looks good?"
+
+#=> 1 go slow
+set -v  # prints each statement here, including comments
+trap read debug  # puts a read request after each executable line
 
 #=> 2 time
 # time zone
