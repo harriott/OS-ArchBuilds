7bcbe1ed108f366e06e0e8171843ff61f5956f6f
Joseph Harriott <trohib@gmail.com>
2022-08-03 08:11:52 +0200

    ClamAV

--- build-scripts/16-sudo-configure_freshclam.sh
+++ build-scripts/16-sudo-configure_freshclam.sh
@@ -3,20 +3,20 @@
 set -v  # prints each statement here, including comments
 trap read debug  # puts a read request after each executable line
 
-#=> ClamAV 1 empty sock file
-# as freshclam triggered warning "Clamd was NOT notified"
-touch /run/clamav/clamd.ctl
-chown clamav:clamav /run/clamav/clamd.ctl
-freshclam
+# #=> ClamAV 0 empty sock file
+# # as freshclam triggered warning "Clamd was NOT notified"
+# touch /run/clamav/clamd.ctl
+# chown clamav:clamav /run/clamav/clamd.ctl
+# freshclam
 
-#=> ClamAV 2 freshclam daemon - enable
+#=> ClamAV 1 freshclam daemon 0 enable
 systemctl enable clamav-freshclam.service --now
 systemctl status clamav-freshclam.service
 
-#=> ClamAV 3 freshclam daemon - stop
-systemctl stop clamav-freshclam.service
+# #=> ClamAV 1 freshclam daemon 1 stop
+# systemctl stop clamav-freshclam.service
 
-#=> ClamAV 3 test
+#=> ClamAV 2 test
 trap - debug  # first turn off debug
 #  be patient after the curl
 curl https://secure.eicar.org/eicar.com.txt | clamscan -
