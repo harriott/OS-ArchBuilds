f09cdf599dc8fe9697ce0a2f1ac954149144cbb7
Joseph Harriott <trohib@gmail.com>
2022-08-14 21:20:59 +0200

    no e for exa

--- build-scripts/2-to_X/1-configure_jo.sh
+++ build-scripts/2-to_X/1-configure_jo.sh
@@ -5,26 +5,23 @@
 read -p "\$ARCHBUILDS is $ARCHBUILDS - looks good?"
 
 #=> 1 go slow
-set -v  # prints each statement here, including comments
+set -ev
 trap read debug  # puts a read request after each executable line
 
-#=> 2 broot br
-broot  # to load  br
-
-#=> 2 for *mutt
-m=~/.local/share/mail
-mkdir $m $m/cz $m/fm $m/tj $m/live $m/parl $m/troh $m/gmx $m/zou
+# #=> 2 for *mutt
+# m=~/.local/share/mail
+# mkdir $m $m/cz $m/fm $m/tj $m/live $m/parl $m/troh $m/gmx $m/zou
 
 # #=> 2 for my m4p scripts
-mkdir -p ~/texmf/tex/latex/m4p  # needed eventually for Pandoc in $Bash/bashrc-ob
+# mkdir -p ~/texmf/tex/latex/m4p  # needed eventually for Pandoc in $Bash/bashrc-ob
 
 # #=> 2 grab default xinitrc
-cp /etc/X11/xinit/xinitrc ~/xinitrc-m8e
+# cp /etc/X11/xinit/xinitrc ~/xinitrc-m8e
 
-#=> 2 LanguageTool
-# needed for command-line usage
-sudo ln -sf $cITh/CP/LanguageTool /usr/share/java/LanguageTool
-e -la /usr/share/java
+# #=> 2 LanguageTool
+# # needed for command-line usage
+# sudo ln -sf $cITh/CP/LanguageTool /usr/share/java/LanguageTool
+# exa -la /usr/share/java
 
 #=> 2 make AUR directories
 mkdir ~/Arch/AUR ~/Arch/AURdev-clone ~/Arch/AURdev-make
