227e5fc54a7b34eec99cf72e8e911f1b6fe23b03
Joseph Harriott <trohib@gmail.com>
2022-08-18 19:57:59 +0200

    $pm

--- build-scripts/anytime/updates.sh
+++ build-scripts/anytime/updates.sh
@@ -3,27 +3,31 @@
 
 set -e
 
-#=> 0 save update log
-echo 'checkupdates'
-checkupdates > $ulL/Arch/$host/pacman/checkupdates-$(date '+%y%m%d-%H%M').log
+#=> 0 pacman directory
+pm=$ulL/Arch/$host/pacman
+[ -d $pm ] || pm=~/pacman
 
-#=> 1 updates
+#=> 1 save update log
+echo 'checkupdates'
+checkupdates > $pm/checkupdates-$(date '+%y%m%d-%H%M').log
+
+#=> 2 updates
 echo 'Do updates:'
 sudo pacman -Syu
 ################################
 ## reboot if kernel updated ! ##
 ################################
 
-#=> 5 pacnews
+#=> 3 pacnews
 echo 'Checking for pacnews:'
 sudo find /etc -type f -name "*pacnew"
 read -p "- were there?"
 
-#=> 5 save full query
+#=> 7 save full query
 echo 'saving full package log'
-pacman -Q > $ulL/Arch/$host/pacman/pacman-Q-$(date '+%y%m%d-%H%M').log
+pacman -Q > $pm/pacman-Q-$(date '+%y%m%d-%H%M').log
 
-#=> 7 /var/cache/pacman/pkg
+#=> 5 /var/cache/pacman/pkg
 echo 'minimising /var/cache/pacman/pkg'
 if [ $host = "LIP120s81A4" ]; then
   yes | sudo pacman -Scc  # empty completely
@@ -31,7 +35,7 @@ else
   sudo paccache -qr  # reduce to last 3 versions
 fi
 
-#=> 8 AURs
+#=> 6 AURs
 echo 'checking AURs'
 if ( pacman -Qs auracle > /dev/null 2>&1) ; then
   auracle sync
