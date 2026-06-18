b3958c6e7bd67ed0a7eedf1e60e15d4a3aab86f3
Joseph Harriott <trohib@gmail.com>
2026-06-18 18:28:10 +0200

    Firewalld

--- bs-1-into_jo/5-sudo-toFirewall.sh
+++ bs-1-into_jo/5-sudo-toFirewall.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# from root,  bash $OSAB/bs-1-into_jo/5-sudo-toFirewall.sh
+
 if [[ $(id -u) > 0 ]]; then echo "Run this as root!"; exit; fi
 
 set -ev  # quits on error, prints each statement here, including comments
@@ -7,16 +9,16 @@ set -ev  # quits on error, prints each statement here, including comments
 #=> 0 check $OSAB
 read -p "\$OSAB is $OSAB - looks good?"
 
-#=> 2 ClamAV 0 install
-freshclam  # takes a long time to update the database!
+# #=> 2 ClamAV 0 install
+# freshclam  # takes a long time to update the database!
 
-#=> 2 Firewalld 0
-# 1 get & enable
-pacman -S firewalld
-systemctl enable firewalld --now
+# #=> 2 Firewalld 0
+# # 1 get & enable
+# pacman -S firewalld
+# systemctl enable firewalld --now
 
-# 2 active zone
-firewall-cmd --set-default-zone=home
+# # 2 active zone
+# firewall-cmd --set-default-zone=home
 
 #=> 2 Firewalld 1 port for Avahi
 # open UDP port 5353
